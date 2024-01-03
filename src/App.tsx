ff8d03946edc5c732b5cc7f4bf8a6e424682bfcf
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-01-03 21:41:30 +0100

    Modified contents of bg-text-div to suit design

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,26 @@ import cart from "./images/cart.png"
 function App() {
   return (
     <div className="wrapper">
-      <nav style={{ backgroundColor: "#212121", display: "flex" }}>
-        <div>
-          <img src={zaron} alt="zaronLogo" />
-        </div>
-        <div className='right-navbar'>
-          <div className='navLinks'>
-            <a href="">Makeup</a>
-            <a href="">Skincare</a>
-            <a href="">Explore</a>
-            <a href="">Wishlist</a>
-          </div>
+      <div>
+        <nav style={{ backgroundColor: "#212121", display: "flex" }}>
           <div>
-            <img className='nav-icon' src={search} alt="search-icon" />
-            <img className='nav-icon' src={user} alt="user-icon" />
-            <img className='nav-icon' src={cart} alt="cart-icon" />
+            <img src={zaron} alt="zaronLogo" />
           </div>
-        </div>
-      </nav>
+          <div className='right-navbar'>
+            <div className='navLinks'>
+              <a href="">Makeup</a>
+              <a href="">Skincare</a>
+              <a href="">Explore</a>
+              <a href="">Wishlist</a>
+            </div>
+            <div>
+              <img className='nav-icon' src={search} alt="search-icon" />
+              <img className='nav-icon' src={user} alt="user-icon" />
+              <img className='nav-icon' src={cart} alt="cart-icon" />
+            </div>
+          </div>
+        </nav>
+      </div>
       <hr style={{ background: "#EAEAEA" }} />
       <div className='bg-div'>
         <div className='bg-text-div'>
