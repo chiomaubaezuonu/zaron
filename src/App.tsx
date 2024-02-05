06a990f88fcab95754bd8875be3b99b7389908ef
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-02-05 10:36:58 +0100

    Added all the sub dropdowns for makeup and skincare links onthe navbar

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import section5Img from "./images/section5-img.png"
 import { Button, Input, Layout } from 'antd';
 import { Footer } from 'antd/es/layout/layout';
 import arrowdown from "./images/arrow-chevron-down.svg"
+import rightArrow from "./images/chevron-right-arrow.svg"
 
 function App() {
 
@@ -39,7 +40,7 @@ function App() {
                   <div className='nav-dropdown-div'>
                     <ul className='nav-dropdown'>
                       <li className='face-menu'>Face
-                        <img src={arrowdown} alt="down-arrow" className='menu-item-arrow' />
+                        <img src={rightArrow} alt="down-arrow" className='menu-item-arrow' />
                         <div className='menuItem-dropdown-div'>
                           <ul className='nav-dropdown'>
                             <li>Foundation</li>
@@ -48,7 +49,25 @@ function App() {
                         </div>
                       </li>
                       <li>Eyes</li>
-                      <li>Lips</li>
+                      <li className='lips-menu'>Lips
+                        <div className='menuItem-dropdown-div'>
+                          <ul className='nav-dropdown'>
+                            <li>Lipstick</li>
+                            <li>Lip Balm</li>
+                            <li className='lipGloss-menu'>Lip Gloss
+                              <div className='lipGloss-menuItem-dropdown'>
+                                <ul className='nav-dropdown'>
+                                  <li>Luxurious Lip Gloss</li>
+                                  <li>Maxsheen Lip Gloss</li>
+                                  <li>Sheer Lip Gloss</li>
+                                </ul>
+                              </div>
+                            </li>
+                            <li>Lip Liner</li>
+                            <li>Matte Lipfix</li>
+                          </ul>
+                        </div>
+                      </li>
                       <li>Accessories</li>
                     </ul>
                   </div>
@@ -57,8 +76,24 @@ function App() {
                   <img src={arrowdown} alt="down-arrow" className='arrowdown' />
                   <div className='nav-dropdown-div'>
                     <ul className='nav-dropdown'>
-                      <li>Men</li>
-                      <li>Women</li>
+                      <li className='face-menu'>Men
+                        <img src={rightArrow} alt="down-arrow" className='menu-item-arrow' />
+                        <div className='menuItem-dropdown-div'>
+                          <ul className='nav-dropdown'>
+                            <li>Body</li>
+                            <li>Face</li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li className='face-menu'>Women
+                        <img src={rightArrow} alt="down-arrow" className='menu-item-arrow' />
+                        <div className='menuItem-dropdown-div'>
+                          <ul className='nav-dropdown'>
+                            <li>Body</li>
+                            <li>Face</li>
+                          </ul>
+                        </div>
+                      </li>
                     </ul>
                   </div>
                 </a>
@@ -78,7 +113,7 @@ function App() {
                 <img className='nav-icon' src={search} alt="search-icon" />
                 <img className='nav-icon' src={user} alt="user-icon" />
                 {/* <span className='shopping-cart' style={{display:"flex"}}> */}
-                <img style={{position: 'relative'}} className='nav-icon' src={cart} alt="cart-icon" />
+                <img style={{ position: 'relative' }} className='nav-icon' src={cart} alt="cart-icon" />
                 <span className='shopping-cart-count'>0</span>
                 {/* </span> */}
               </div>
