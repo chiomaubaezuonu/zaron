5b2997335abbe015e3d47c86b095fffcbc28c8d5
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-02-28 04:00:15 +0100

    Added lipstick images to the lipstick cards.

--- src/Pages/lipstickData.tsx
+++ src/Pages/lipstickData.tsx
@@ -8,37 +8,39 @@
     },
     {
         id:2,
-        image: "/images/boss-lady-matte-lipstick.jpg",
+        image: "/images/butterscotch-lipstick.jpg",
         name: "Zaron Butterscotch Matte Lipstick",
         price: "5500"
     },
     {
         id:3,
-        image: "/images/boss-lady-matte-lipstick.jpg",
+        image: "/images/passion-lipstick.jpg",
         name: "Zaron Passion Matte Lipstick",
         price: "5500"
     },
     {
         id:4,
-        image: "/images/boss-lady-matte-lipstick.jpg",
+        image: "/images/temptress-lipstick.jpg",
         name: "Zaron Temptress Matte Lipstick",
         price: "5500"
     },
     {
         id:5,
-        image: "/images/boss-lady-matte-lipstick.jpg",
+        image: "/images/cowgirl-lipstick.jpg",
         name: "Zaron Cowgirl Matte Lipstick",
         price: "5500"
     },
+   
+   
     {
         id:6,
-        image: "/images/boss-lady-matte-lipstick.jpg",
+        image: "/images/Vegas-lipstick.jpg",
         name: "Zaron Matte Lipstick- Vegas",
         price: "5500"
     },
     {
         id:7,
-        image: "/images/boss-lady-matte-lipstick.jpg",
+        image: "images/traffic-jam-lipstick.jpg",
         name: "Zaron Traffic Jam Matte Lipstick",
         price: "5500"
     }
